f4133a1e465c95898bcaf0e01778a0e46f29ac6c
Sandro <Sandro.bonomo@gmx.de>
2026-05-19 11:18:42 +0200

    fix: open projects with click on project-container.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function openFirstProject() {
+    window.location.href = "../html/project-one.html";
+}
+
+function openSecondProject() {
+    window.location.href = "../html/project-two.html";
+}
+
+function openThirdProject() {
+    window.location.href = "../html/project-three.html";
+}
