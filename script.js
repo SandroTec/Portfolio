9a231b62b5b083a8b84a3ca48b1570fb3f2f9bcc
Sandro <Sandro.bonomo@gmx.de>
2026-05-21 10:00:09 +0200

    feat: footer for colleague's sticker+ footer-link, feat: linkedIN-icon black and white

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ function openSecondProject() {
 function openThirdProject() {
     window.location.href = "../html/project-three.html";
 }
+
+const link = document.getElementById('footerImage');
+
+  link.addEventListener('mouseenter', () => {
+    link.href = './assets/img/03_Stickers/02Testimonials/linkedIN-Icon-sticker-hover.png';
+  });
+
+  link.addEventListener('mouseleave', () => {
+    link.href = './assets/img/03_Stickers/02Testimonials/linkedIN-Icon-sticker.png';
+  });
