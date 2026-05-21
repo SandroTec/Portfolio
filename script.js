d8513741f38ff308c63f2109521505b2f2457b7e
Sandro <Sandro.bonomo@gmx.de>
2026-05-21 13:00:34 +0200

    fix: skill-sticker text and peel-effect

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 function openFirstProject() {
     window.location.href = "../html/project-one.html";
 }
@@ -20,3 +21,33 @@ const link = document.getElementById('footerImage');
   link.addEventListener('mouseleave', () => {
     link.href = './assets/img/03_Stickers/02Testimonials/linkedIN-Icon-sticker.png';
   });
+
+const sticker = document.getElementById('skillSticker');
+const stickerDefault = './assets/img/03_Stickers/01_Skills - Peel off/Default.png';
+const appetizer = document.getElementById('appetizer')
+const stickerTransition = './assets/img/03_Stickers/01_Skills - Peel off/Transition.png';
+const stickerPeeledOff = './assets/img/03_Stickers/01_Skills - Peel off/Final.png';
+const furtureSkills = document.getElementById('futureSkills');
+
+function peelSticker() {
+    const currentSrc = sticker.getAttribute('src');
+
+    if (currentSrc === stickerDefault) {
+        sticker.src = stickerTransition;
+        
+        setTimeout(() => {
+            sticker.src = stickerPeeledOff;
+            appetizer.classList.add('d-none');
+            furtureSkills.classList.remove('d-none');
+        }, 40); 
+
+    } else if (currentSrc === stickerPeeledOff) {
+        sticker.src = stickerTransition;
+        
+        setTimeout(() => {
+            sticker.src = stickerDefault;
+            appetizer.classList.remove('d-none');
+            furtureSkills.classList.add('d-none');
+        }, 40); 
+    }
+}
