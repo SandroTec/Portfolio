72a36d09e2637bbfcbdb3c34e33271f33d310165
Sandro <Sandro.bonomo@gmx.de>
2026-06-01 14:13:29 +0200

    fix: mobile header toggle btn, fix: responsivness

--- js/lang.helper.js
+++ js/lang.helper.js
@@ -1,25 +1,28 @@
-const langDeButton = document.getElementById('langDeButton');
-const langEnButton = document.getElementById('langEnButton');
+// Ich nenne die Variablen "Buttons" (Mehrzahl), damit klar ist, dass es Listen sind
+const langDeButtons = document.querySelectorAll('.lang-DE-btn');
+const langEnButtons = document.querySelectorAll('.lang-EN-btn');
 const body = document.body;
 
 function setLanguageToGerman() {
   body.classList.remove('lang-en-active');
   document.documentElement.lang = 'de';
   sessionStorage.setItem('preferred-lang', 'de'); 
-  langEnButton.style.color = '#F8F9FA';
-  langDeButton.style.color = ' #F87A55';
+  
+  langEnButtons.forEach(btn => btn.style.color = '#F8F9FA');
+  langDeButtons.forEach(btn => btn.style.color = '#F87A55');
 }
 
 function setLanguageToEnglish() {
   body.classList.add('lang-en-active');
   document.documentElement.lang = 'en';
   sessionStorage.setItem('preferred-lang', 'en');
-  langEnButton.style.color = '#F87A55';
-  langDeButton.style.color = '#F8F9FA';
+  
+  langEnButtons.forEach(btn => btn.style.color = '#F87A55');
+  langDeButtons.forEach(btn => btn.style.color = '#F8F9FA');
 }
 
-langDeButton.addEventListener('click', setLanguageToGerman);
-langEnButton.addEventListener('click', setLanguageToEnglish);
+langDeButtons.forEach(btn => btn.addEventListener('click', setLanguageToGerman));
+langEnButtons.forEach(btn => btn.addEventListener('click', setLanguageToEnglish));
 
 const savedLang = sessionStorage.getItem('preferred-lang');
 
@@ -28,6 +31,3 @@ if (savedLang === 'en') {
 } else {
   setLanguageToGerman();
 }
-
-langDeButton.addEventListener('click', setLanguageToGerman);
-langEnButton.addEventListener('click', setLanguageToEnglish);
--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//mobile header
+const mobileHeader = document.querySelector('.mobile-header');
+const toggleMobileMenu = document.getElementById('toggleMobileMenu');
+const menuImage = document.querySelector('.menu-image');
+
+toggleMobileMenu.addEventListener('click', () => {
+    if (mobileHeader.style.display === 'flex') {
+        mobileHeader.style.display = 'none';
+        menuImage.src = './assets/img/';
+    } else {
+        mobileHeader.style.display = 'flex';
+        menuImage.src = './assets/img/';
+    }
+});
 
 //Open Projectsites
 function openFirstProject() {
