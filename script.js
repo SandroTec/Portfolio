886ff4b59005710940e16b9e5237c91d29e03275
Sandro <Sandro.bonomo@gmx.de>
2026-06-01 21:31:44 +0200

    feat: mobile header button images

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const menuImage = document.querySelector('.menu-image');
 toggleMobileMenu.addEventListener('click', () => {
     if (mobileHeader.style.display === 'flex') {
         mobileHeader.style.display = 'none';
-        menuImage.src = './assets/img/';
+        menuImage.src = './assets/img/04_social_icons/01_Menu.png';
+        
     } else {
         mobileHeader.style.display = 'flex';
-        menuImage.src = './assets/img/';
+        menuImage.src = './assets/img/04_social_icons/Close-icon.png';
     }
 });
 
