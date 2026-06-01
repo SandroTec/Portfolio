5a4a9c7c4f149a582ae08afd0d3f72f8e631151a
Sandro <Sandro.bonomo@gmx.de>
2026-06-01 14:27:48 +0200

    fix: vector displaying for lang switch

--- js/lang.helper.js
+++ js/lang.helper.js
@@ -2,12 +2,15 @@
 const langDeButtons = document.querySelectorAll('.lang-DE-btn');
 const langEnButtons = document.querySelectorAll('.lang-EN-btn');
 const body = document.body;
+const vectorBubbles = document.querySelectorAll('.vector-bubble');
 
 function setLanguageToGerman() {
   body.classList.remove('lang-en-active');
   document.documentElement.lang = 'de';
   sessionStorage.setItem('preferred-lang', 'de'); 
-  
+  vectorBubbles.forEach(bubble => {
+    bubble.style.marginLeft = '20px';
+  });
   langEnButtons.forEach(btn => btn.style.color = '#F8F9FA');
   langDeButtons.forEach(btn => btn.style.color = '#F87A55');
 }
@@ -16,7 +19,10 @@ function setLanguageToEnglish() {
   body.classList.add('lang-en-active');
   document.documentElement.lang = 'en';
   sessionStorage.setItem('preferred-lang', 'en');
-  
+  vectorBubbles.forEach(bubble => {
+    bubble.style.marginLeft = '-35px';
+  });
+
   langEnButtons.forEach(btn => btn.style.color = '#F87A55');
   langDeButtons.forEach(btn => btn.style.color = '#F8F9FA');
 }
