33c9e7eb4670e2f9b9531f8431d210c85841c58f
Sandro <Sandro.bonomo@gmx.de>
2026-06-02 14:18:28 +0200

    fix: fixxing project sites header and mobile responsivness

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-//mobile header
-const mobileHeader = document.querySelector('.mobile-header');
-const toggleMobileMenu = document.getElementById('toggleMobileMenu');
-const menuImage = document.querySelector('.menu-image');
 
-toggleMobileMenu.addEventListener('click', () => {
-    if (mobileHeader.style.display === 'flex') {
-        mobileHeader.style.display = 'none';
-        menuImage.src = './assets/img/04_social_icons/01_Menu.png';
-        
-    } else {
-        mobileHeader.style.display = 'flex';
-        menuImage.src = './assets/img/04_social_icons/Close-icon.png';
-    }
-});
 
 //Open Projectsites
 function openFirstProject() {
