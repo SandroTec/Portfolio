2db37a65b9393ef8284153fceef0216f761d7a98
Sandro <Sandro.bonomo@gmx.de>
2026-06-02 23:10:41 +0200

    fix: update projectsite header for projectsites.

--- js/toggle.mobile.helper.js
+++ js/toggle.mobile.helper.js
@@ -1,15 +1,33 @@
-//mobile header
 const mobileHeader = document.querySelector('.mobile-header');
 const toggleMobileMenu = document.getElementById('toggleMobileMenu');
 const menuImage = document.querySelector('.menu-image');
 
+toggleMobileMenu.addEventListener('mouseenter', () => {
+    const isMenuOpen = mobileHeader.style.display === 'flex';
+    
+    if (isMenuOpen) {
+        menuImage.src = '../assets/img/04_social_icons/Close-icon-hover.png';
+    } else {
+        menuImage.src = '../assets/img/04_social_icons/Menu-Hover.png';
+    }
+});
+
+toggleMobileMenu.addEventListener('mouseleave', () => {
+    const isMenuOpen = mobileHeader.style.display === 'flex';
+    
+    if (isMenuOpen) {
+        menuImage.src = '../assets/img/04_social_icons/Close-icon.png';
+    } else {
+        menuImage.src = '../assets/img/04_social_icons/01_Menu.png';
+    }
+});
+
 toggleMobileMenu.addEventListener('click', () => {
     if (mobileHeader.style.display === 'flex') {
         mobileHeader.style.display = 'none';
-        menuImage.src = '../assets/img/04_social_icons/01_Menu.png';
-        
+        menuImage.src = '../assets/img/04_social_icons/01_Menu.png'; 
     } else {
         mobileHeader.style.display = 'flex';
-        menuImage.src = '../assets/img/04_social_icons/Close-icon.png';
+        menuImage.src = '../assets/img/04_social_icons/Close-icon-hover.png'; 
     }
 });
