cc2af6b3cb1c93edf4e13b0c56069e4202c587e8
Sandro <Sandro.bonomo@gmx.de>
2026-06-09 11:11:49 +0200

    fix: mobile header for legal notice and privacy policy

--- js/toggle.mobile.helper.js
+++ js/toggle.mobile.helper.js
@@ -2,6 +2,8 @@ const mobileHeader = document.querySelector('.mobile-header');
 const toggleMobileMenu = document.getElementById('toggleMobileMenu');
 const menuImage = document.querySelector('.menu-image');
 
+const headerBG = document.getElementById('bg-image');
+
 toggleMobileMenu.addEventListener('mouseenter', () => {
     const isMenuOpen = mobileHeader.style.display === 'flex';
     
@@ -23,11 +25,20 @@ toggleMobileMenu.addEventListener('mouseleave', () => {
 });
 
 toggleMobileMenu.addEventListener('click', () => {
-    if (mobileHeader.style.display === 'flex') {
-        mobileHeader.style.display = 'none';
-        menuImage.src = '../assets/img/04_social_icons/01_Menu.png'; 
-    } else {
+    if (mobileHeader.style.display === 'none' || mobileHeader.style.display === '') {
         mobileHeader.style.display = 'flex';
         menuImage.src = '../assets/img/04_social_icons/Close-icon-hover.png'; 
+        if (window.location.pathname.includes("legal-notice.html") || window.location.pathname.includes("privacy-policy.html")) {
+            headerBG.classList.remove('header-bg');
+        }
+    } else {
+        mobileHeader.style.display = 'none';
+        menuImage.src = '../assets/img/04_social_icons/01_Menu.png'; 
+        if (window.location.pathname.includes("legal-notice.html") || window.location.pathname.includes("privacy-policy.html"))  {
+            headerBG.classList.add('header-bg');
+        }
     }
 });
+
+
+
